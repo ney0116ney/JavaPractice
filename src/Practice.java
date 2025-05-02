2db38f95c73ab3ad60f7e88edf02180b7d9e445f
Laney Tsai <ingenuous0116@gmail.com>
2025-05-02 15:46:48 +0800

    BMI計算器

---
+++ src/Practice.java
@@ -0,0 +1,47 @@
+//import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        /*Scanner scanner = new Scanner(System.in);
+
+         // 練習一. 
+        
+        //製作BMI計算機，BMI指數等於體重(公斤)除以身高(公尺)的平方
+        // BMI = 體重(公斤) / 身高(公尺)的平方
+
+        
+        
+        //用身高公分來算。
+        System.out.println("請輸入您的身高(公分): ");
+        double heightInCm = scanner.nextDouble(); // 讀取身高（公分）
+
+        // 將身高從公分轉換為公尺
+        double heightInMeters = heightInCm / 100;
+
+
+        System.out.println("輸入您的體重(公斤): ");
+        int weight = scanner.nextInt();
+
+    
+
+        // 計算 BMI--使用身高(公分)的平方
+        double bmi = weight / (heightInMeters * heightInMeters);
+        System.out.println("您的 BMI 指數為: " + bmi);
+
+        if(bmi < 18.5) {
+            System.out.println("您屬於體重過輕。");
+        } else if (bmi >= 18.5 && bmi < 24) {
+            System.out.println("您屬於正常範圍。");
+        } else if (bmi >= 24 && bmi < 27) {
+            System.out.println("您屬於過重。");
+        } else {
+            System.out.println("您屬於肥胖。");
+        }
+        
+        scanner.close(); // 關閉 Scanner */
+
+
+    }
+}

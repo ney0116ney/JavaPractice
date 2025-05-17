2ce6569c8bb76adeb707d5c3716ff16dc47d4e21
Ney <ingenuous0116@gmail.com>
2025-05-17 16:15:28 +0800

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,21 @@
+public class PersonInfo {
+    
+    public static void main(String[ ]args){
+
+    //顯示在同一行    
+    System.out.print("姓名:小明");
+    System.out.print("年齡:20歲");
+    System.out.print("職業:學生");
+    
+    System.out.println("");
+    //分成三行顯示
+    System.out.println("姓名:小明");
+    System.out.println("年齡:20歲");
+    System.out.println("職業:學生");
+
+    
+    }
+
+
+
+}
